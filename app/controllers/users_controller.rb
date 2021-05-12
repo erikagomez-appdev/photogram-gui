454b16711dda78e6a0f6f09bb0b57753660bdddd
erikag213 <erika.gomez@chicagobooth.edu>
2021-05-12 03:41:52 +0000

    user homepage

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+     matching_users = User.all
+      @list_of_users = matching_users.order({ :username => :asc })
+    render({ :template => "users_templates/index.html.erb" })
+  end
+
+  def show
+     
+    render({ :template => "users_templates/show.html.erb" })
+  end
+
+end
