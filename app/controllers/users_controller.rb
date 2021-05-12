5fbc4e09898e3e908b50b7de2e537aaecee41805
erikag213 <erika.gomez@chicagobooth.edu>
2021-05-12 04:30:40 +0000

    progress user index and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,18 @@ class UsersController < ApplicationController
   end
 
   def show
-     
-    render({ :template => "users_templates/show.html.erb" })
+    #Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.at(0)
+
+    #if @the_user == nil
+      #redirect_to("/404")
+      #else 
+      render({ :template => "users_templates/show.html.erb" })
+    #end 
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index" })
 
-  get("/users/:username", { :controller => "users", :action => "show" })
+  get("/users/:path_username", { :controller => "users", :action => "show" })
 
 end
